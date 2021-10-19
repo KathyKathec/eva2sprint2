1fea3ffb585bd865ad882f8b897f63bf8ee4f4f8
Katherin} <katherinpereda19@hotmail.com>
2021-10-19 13:57:09 -0300

    Primeiro commit

---
+++ src/TestRemocao.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestRemocao {
+
+	public static void main(String[] args) throws SQLException {
+
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.recuperarConexao();
+		
+		Statement stm = connection.createStatement();
+		stm.execute("DELETE FROM PRODUTO WHERE ID >3");
+		
+		Integer linhasModificadas = stm.getUpdateCount();
+		
+		System.out.println("Quantidade de linhas removidas:" + linhasModificadas);
+		
+		
+	}
+
+}
